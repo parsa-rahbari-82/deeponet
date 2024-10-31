b4474e4aa4771e90ef076324ad27a052ba76df3c
Parsa Rahbari <prahbari82@gmail.com>
2024-10-31 14:43:30 +0330

    fix bug for dynamic import.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,11 @@
-
+import glob
 import os
-import pkgutil
 
-# Automatically import all modules in the current package
-__all__ = []
+# Get the current directory
+module_dir = os.path.dirname(__file__)
 
-# Iterate through all modules in the package
-for _, module_name, _ in pkgutil.iter_modules(__path__):
-    __all__.append(module_name)
-    # Dynamically import the module
-    __import__(f'.{module_name}', locals(), globals())
+# Import all .py files in the current directory (excluding __init__.py)
+for module_file in glob.glob(os.path.join(module_dir, '*.py')):
+    module_name = os.path.basename(module_file)[:-3]  # Strip the .py extension
+    if module_name != '__init__':
+        exec(f'from .{module_name} import *')
